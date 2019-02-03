5d52dbb921784e846b14c74689b2d08ae01b0439
rameshkunwar1 <46709549+rameshkunwar1@users.noreply.github.com>
2019-02-03 13:44:27 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+AOS.init({
+    duration: 2000,
+    once: true,
+    mirror: true,
+    easing: "ease-in-out"
+});
+/*==============================
+     PRELOADER
+   ==============================*/
+const preloader = document.querySelector("#preloader");
+const status = document.querySelector("#status");
+
+window.addEventListener('load', function () {
+    preloader.style.display = "none";
+    status.style.display = "none";
+
+})
+
+/*==============================
+    END OF   PRELOADER
+    ==============================*/
+
+/*===================================
+       team section
+==================================*/
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 600,
+        loop: true,
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            600: {
+                items: 2
+
+            },
+            // breakpoint from 768 up
+            900: {
+                items: 3
+
+
+            }
+        }
+    });
+});
+// client riview carousel
+$(function () {
+    $(".client-container").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 600,
+        loop: true
+    });
+});
